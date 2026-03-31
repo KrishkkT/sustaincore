ed7e741a58e45f1c864529eaede176758b818f6b
KrishkkT <krishthakker508@gmail.com>
2026-03-31 23:17:59 +0530

    Updated

--- src/js/components.js
+++ src/js/components.js
@@ -36,9 +36,6 @@ export const header = `
         </a>
 
         <a href="/clearances-eia.html" class="mega-item">
-          <div class="icon">
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"/></svg>
-          </div>
           <div>
             <span class="title">GPCB Compliance & Env. Approval</span>
             <span class="desc">EC, EIA, Consent management & EPR.</span>
